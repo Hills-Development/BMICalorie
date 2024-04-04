a09daed884aedf0714cee49674113f5eba2c61bb
Lehrshare <lehrsharedev@gmail.com>
2024-04-04 14:23:25 +0200

    Added a message on the BMI Side which says what category of BMI you are

--- public/js/bmi.js
+++ public/js/bmi.js
@@ -18,11 +18,27 @@ document.addEventListener('DOMContentLoaded', function() {
             bmi = weight / (height/100 * height/100) *1.05;
             console.log(weight / (height * height)*1.05)
         }
-        
+
+        let category;
     
         const result = bmi.toFixed(2);
+
+        if (result <= 18.5) {
+            category = 'Underweight';
+        } else if (result <= 25) {
+            category = 'Healthy Weight';
+        } else if (result <= 29.9) {
+            category = 'Overweight';
+        } else {
+            category = 'Obese';
+        }
+        
+
+        console.log(category);
+
         document.getElementById('result').innerText = result;
         document.getElementById('topText').style += 'display:block';
+        document.getElementById('bottomText').innerText = 'You are: ' + category;
         document.getElementById('bottomText').style += 'display:block';
     }) 
 })
